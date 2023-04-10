cf0bbdc87baac9972b32210842c1076979f416f0
soulteary <soulteary@gmail.com>
2023-04-10 22:37:12 +0800

    chore: improve installer

--- example/installer-standalone.sh
+++ example/installer-standalone.sh
@@ -1,13 +1,13 @@
 #!/bin/env bash
 
 # install dependencies
-sudo apt-get install -y libsecret-tools
+sudo apt-get install -y libsecret-tools wget
 
 # download urch
 URCH_VER=1.6.0 \
-wget https://github.com/soulteary/ubuntu-remote-control-helper/releases/download/v${URCH_VER}/urch_${URCH_VER}_linux_amd64.tar.gz && \
-tar zxvf urch_${URCH_VER}_linux_amd64.tar.gz
-rm -rf zxvf urch_${URCH_VER}_linux_amd64.tar.gz
+wget "https://github.com/soulteary/ubuntu-remote-control-helper/releases/download/v${URCH_VER}/urch_${URCH_VER}_linux_amd64.tar.gz" && \
+tar zxvf "urch_${URCH_VER}_linux_amd64.tar.gz" && \
+rm -rf "urch_${URCH_VER}_linux_amd64.tar.gz"
 
 # copy urch to executable directory
 sudo mv urch /usr/local/bin/
--- example/installer.sh
+++ example/installer.sh
@@ -1,19 +1,21 @@
 #!/bin/env bash
 
 # install dependencies
-sudo apt-get install -y libsecret-tools supervisor xserver-xorg-core xserver-xorg-video-dummy xvfb dbus-x11
+sudo apt-get install -y wget libsecret-tools supervisor xserver-xorg-core xserver-xorg-video-dummy xvfb dbus-x11
 
 # download urch
 URCH_VER=1.6.0 \
-wget https://github.com/soulteary/ubuntu-remote-control-helper/releases/download/v${URCH_VER}/urch_${URCH_VER}_linux_amd64.tar.gz && \
-tar zxvf urch_${URCH_VER}_linux_amd64.tar.gz
-rm -rf zxvf urch_${URCH_VER}_linux_amd64.tar.gz
+wget "https://github.com/soulteary/ubuntu-remote-control-helper/releases/download/v${URCH_VER}/urch_${URCH_VER}_linux_amd64.tar.gz" && \
+tar zxvf "urch_${URCH_VER}_linux_amd64.tar.gz" && \
+rm -rf "urch_${URCH_VER}_linux_amd64.tar.gz"
 
 # copy urch to executable directory
 sudo mv urch /usr/local/bin/
 
 # download supervisor conf
-curl -sSL https://github.com/soulteary/ubuntu-remote-control-helper/raw/main/example/supervisor-urch.conf -o /etc/supervisor/conf.d/urch.conf
+wget https://github.com/soulteary/ubuntu-remote-control-helper/raw/main/example/supervisor-urch.conf
+sudo mv supervisor-urch.conf /etc/supervisor/conf.d/urch.conf
 
 # download x11 config
-curl -sSL https://github.com/soulteary/ubuntu-remote-control-helper/raw/main/example/x11-xorg.conf -o /etc/X11/xorg.conf
+wget https://github.com/soulteary/ubuntu-remote-control-helper/raw/main/example/x11-xorg.conf
+sudo mv x11-xorg.conf /etc/X11/xorg.conf
