40a434f6626ca128d1e9c2b40a2449f6b38ba323
soulteary <soulteary@gmail.com>
2023-04-10 13:52:04 +0800

    feat: use cli to set username, password

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"strings"
@@ -13,12 +14,15 @@ const (
 	DEFAULT_UBUNTU_REMOTE_DESKTOP_RDP = `org.gnome.desktop.remote-desktop.rdp`
 	DEFAULT_UBUNTU_REMOTE_DESKTOP_VNC = `org.gnome.desktop.remote-desktop.vnc`
 	DEFAULT_CRONTAB_INTERVAL          = `@every 1m`
+	DEFAULT_USERNAME                  = "soulteary"
+	DEFAULT_PASSWORD                  = "soulteary"
+	DEFAULT_DAEMON_MODE               = false
 )
 
 var (
-	URCH_USER   = `soulteary`
-	URCH_PASS   = `soulteary`
-	URCH_DAEMON = false
+	URCH_USER   = DEFAULT_USERNAME
+	URCH_PASS   = DEFAULT_PASSWORD
+	URCH_DAEMON = DEFAULT_DAEMON_MODE
 )
 
 // Initialize global variables
@@ -28,17 +32,41 @@ func init() {
 	envUser := strings.TrimSpace(os.Getenv("UBUNTU_REMOTE_USER"))
 	if envUser != "" {
 		URCH_USER = envUser
-		fmt.Println(`set remote username:`, URCH_USER)
+		fmt.Println(`set remote username by env:`, URCH_USER)
 	}
 
 	envPass := strings.TrimSpace(os.Getenv("UBUNTU_REMOTE_PASS"))
 	if envPass != "" {
 		URCH_PASS = envPass
-		fmt.Println(`set remote password:`, URCH_PASS)
+		fmt.Println(`set remote password by env:`, URCH_PASS)
 	}
 
 	envDaemon := strings.ToLower(strings.TrimSpace(os.Getenv("UBUNTU_DAEMON")))
 	if envDaemon == "true" || envDaemon == "on" || envDaemon == "1" {
+		URCH_DAEMON = true
+	}
+
+	var cliUser string
+	var cliPass string
+	var cliDaemon string
+
+	flag.StringVar(&cliUser, "user", DEFAULT_USERNAME, "set remote control username")
+	flag.StringVar(&cliPass, "pass", DEFAULT_PASSWORD, "set remote control password")
+	flag.StringVar(&cliDaemon, "daemon", "", "let app running in daemon mode")
+	flag.Parse()
+
+	if cliUser != DEFAULT_USERNAME && cliUser != "" {
+		URCH_USER = cliUser
+		fmt.Println(`set remote username by cli:`, URCH_USER)
+	}
+
+	if cliPass != DEFAULT_PASSWORD && cliPass != "" {
+		URCH_PASS = cliPass
+		fmt.Println(`set remote password by cli:`, URCH_USER)
+	}
+
+	cliDaemon = strings.ToLower(strings.TrimSpace(cliDaemon))
+	if cliDaemon == "true" || cliDaemon == "on" || cliDaemon == "1" {
 		URCH_DAEMON = true
 	}
 }
