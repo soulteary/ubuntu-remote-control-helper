9f803e9528f6937cf0d932eafc3da39f87786ae5
CongLiu_CN <upcliucong@gmail.com>
2023-12-17 13:41:22 -0500

    fix syntax error

--- example/installer-standalone.sh
+++ example/installer-standalone.sh
@@ -4,7 +4,7 @@
 sudo apt-get install -y libsecret-tools wget
 
 # download urch
-URCH_VER=1.7.0 \
+URCH_VER=1.7.0
 wget "https://github.com/soulteary/ubuntu-remote-control-helper/releases/download/v${URCH_VER}/urch_${URCH_VER}_linux_amd64.tar.gz" && \
 tar zxvf "urch_${URCH_VER}_linux_amd64.tar.gz" && \
 rm -rf "urch_${URCH_VER}_linux_amd64.tar.gz"
